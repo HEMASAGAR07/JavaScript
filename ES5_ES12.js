b5b3a8739ed6f70248c65d124e0535af91893d46
SANISETTY HEMA SAGAR <93524527+HEMASAGAR07@users.noreply.github.com>
2024-05-31 14:15:30 +0530

    Update and rename JSpractice.js to ES5_ES12.js
    Learnt all the feautures of java

--- ES5_ES12.js
+++ ES5_ES12.js
@@ -1,7 +1,7 @@
 console.log("belive");
 
 
-//javascript mapping operator
+//java script mapping operator
 var array =[1,2,3,45]
 var output =array.map(
     function(element){
@@ -122,3 +122,97 @@ console.log(name)
  so we can get required information without accessing the perosn.var */
 
  //E7 operators
+ //EXponential operators
+//includes()
+let person1 =[1,2,3]
+console.log(person1.includes(2));
+//Exponential operator - for checking power of a number
+//ES8
+//padStart and padend
+let p ='Hello';
+console.log(p.padStart(7,'*'))
+console.log(p.padEnd(7,'*'))
+
+//Object values and object entitities
+let q={
+    name:'sagar',
+    age:20,
+    address:'chennai - tamil nadu'
+
+}
+console.log(Object.entries(q))
+console.log(Object.values(q))
+
+//Async / await
+//When we try to retrive or get an api data there may be a delay so to manage the delay we use this function
+//to handle the http promises request
+
+/*async() =>{
+    const response = await axios.get('https://www.grabon.in/coupon-codes/159733/')
+    console.log(response.data) it is non blocking
+} // it works in an local host */
+
+//Regular expression operators
+let a = "  Hello world "
+console.log(a.match(/[a-z]/g)) // i for camel case , ^ for not operator , g for globally
+
+/* ES 10 functions
+Flat 
+Function.toString 
+trimStart
+trimEnd
+Object.fromEntries */
+
+let a1 = [1 ,2, 3,[2,3,4],[3,5,6,[7,8,9]]]
+// flat funtion is used for flatenning the nested array to flaten array depending up on given length
+// if we give flat(1) it flatents level1 means 1 nested array
+console.log(a1.flat(1))
+console.log(a1.flat(2))
+
+//Function to string - to convert a function to string
+function a12(num){
+    console.log(num);
+}
+console.log(a12.toString())
+
+// Trim start used to trim extra spaces at starting
+
+console.log(a)
+console.log(a.trimStart())
+
+//ES11 Nullish operator
+let e= null;
+let e_o = e ?? 'sag';
+console.log(e_o)
+
+//OPtional chaining is a safe way to access nested object properties , evn if the property doesnt exist
+ 
+/* let pets ={
+    cat:{
+        name : 'void'
+    }
+ }
+ alert(pets.cat.name); excutable in jsbin */
+ //but if we want to access an object which we didnt declare then we using optional chain operator
+
+ let u ={
+    admin(){
+        console.log('I am from admin method')
+    }
+ }
+u.admin?.()
+
+/* ES12 
+ replaceall() function
+ */
+let u2 ='jxvxScript'
+console.log(u2.replaceAll('x','a'))
+//numericssaparator
+let u3=6788_837;
+console.log(u3)
+//logic assignment operators
+// &&= and ||=
+let arr5=10;
+let arr6=20;
+arr5 &&= arr6 // arr2=arr2 && arr1 
+console.log(arr6)
